3a3a5ad9691ea6ae339e934029952d5b376bebd0
Kawser Ahmed <32969466+kawserahmed47@users.noreply.github.com>
2019-10-17 21:48:17 +0600

    Add files via upload

---
+++ precache-manifest.09604455875a88a9b8bd147351dcef7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe1599494cf0738fb957c72296fc379a",
+    "url": "/react_tic_tac_toe/index.html"
+  },
+  {
+    "revision": "e6acc04f74ac44823375",
+    "url": "/react_tic_tac_toe/static/css/main.f5d9c047.chunk.css"
+  },
+  {
+    "revision": "4aab6e712564788423e0",
+    "url": "/react_tic_tac_toe/static/js/2.70b96583.chunk.js"
+  },
+  {
+    "revision": "e6acc04f74ac44823375",
+    "url": "/react_tic_tac_toe/static/js/main.38c5b069.chunk.js"
+  },
+  {
+    "revision": "9a40c8474d216cfc1d09",
+    "url": "/react_tic_tac_toe/static/js/runtime-main.ed2390da.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react_tic_tac_toe/precache-manifest.09604455875a88a9b8bd147351dcef7c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_tic_tac_toe/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
